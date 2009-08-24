2a99fd9d6cdeeda19e6425ee07e1656c571d4f36
Rob Sherwood <rob.sherwood@stanford.edu>
2009-08-24 15:11:00 -0700

    fixed control socket options

--- control.c
+++ control.c
@@ -17,18 +17,29 @@ int setup_control_channel(oflops_context *ctx)
 	char buf[BUFLEN];
 	unsigned int len;
 	long flags;
+	int one;
+
+
 	fprintf(stderr, "Creating server socket...\n");
 	ctx->listen_fd = socket( AF_INET, SOCK_STREAM, 0);
 	if(ctx->listen_fd == -1)
-		perror_and_exit("socket",1);
+		perror_and_exit("Dying on socket",1);
+
+	fprintf(stderr, "Setting SO_REUSE\n");
+	one =1;
+	if(setsockopt(ctx->listen_fd,SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one)))
+		perror_and_exit("Dying on setsockopt(SO_REUSEADDR)\n",1);
+
 	sin.sin_family = AF_INET;
 	sin.sin_addr.s_addr = INADDR_ANY;
 	sin.sin_port = htons(ctx->listen_port);
-	fprintf(stderr, "Listenning on port %d \n", ctx->listen_port);
+	fprintf(stderr, "Binding to port %d \n", ctx->listen_port);
 	if(bind( ctx->listen_fd, (struct sockaddr *) &sin, sizeof(sin)))
-		perror_and_exit("binding listen port",1);
+		perror_and_exit("Dying on binding listen port",1);
 	if(listen( ctx->listen_fd,16))
-		perror_and_exit("listen",1);
+		perror_and_exit("Dying on listen",1);
+
+
 	fprintf( stderr, "Waiting for a switch to connect...\n");
 	len = sizeof(sin);
 	if((ctx->control_fd=
@@ -39,8 +50,8 @@ int setup_control_channel(oflops_context *ctx)
 			buf, htons(sin.sin_port));
 	fprintf( stderr, "Setting control channel to non-blocking\n");
 	flags = O_NONBLOCK;
-	if(fcntl(ctx->control_fd, F_SETFL, &flags))
-		perror_and_exit("fcntl(control, O_NONBLOCK)",1);
+	if(fcntl(ctx->control_fd, F_SETFL, flags))
+		perror_and_exit("Dying on fcntl(control, O_NONBLOCK)",1);
 	return 0;
 }
 
