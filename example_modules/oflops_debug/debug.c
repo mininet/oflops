19650c3866ce1dfe0f4cb17dbbc603233a40bb4b
Rob Sherwood <rob.sherwood@stanford.edu>
2009-09-14 15:14:08 -0700

    return just tcp traffic in module

--- example_modules/oflops_debug/debug.c
+++ example_modules/oflops_debug/debug.c
@@ -95,7 +95,7 @@ int of_event_packet_in(struct oflops_context *ctx, struct ofp_packet_in * pkt_in
 int get_pcap_filter(struct oflops_context *ctx, oflops_channel_name ofc, char * filter, int buflen)
 {
 	if(ofc == OFLOPS_CONTROL)	// only pcap dump the control channel
-		return snprintf(filter,buflen,"port 6633");
+		return snprintf(filter,buflen,"tcp port 6633");
 	else 
 		return 0;
 }
