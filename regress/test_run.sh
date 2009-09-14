fdc77e78ae8afdc34ae2a5dcf80608037b591d69
Rob Sherwood <rob.sherwood@stanford.edu>
2009-09-14 11:46:56 -0700

    force regress to call real binary

--- regress/test_run.sh
+++ regress/test_run.sh
@@ -5,6 +5,6 @@ setup_sleep=10		# !#&!@%$ IPv6 broadcasts for 10 seconds after bringing up an in
 ./test_setup.sh $setup_sleep > /dev/null &
 echo "Waiting $sleep_time seconds for IPv6 Broadcast discovery to go away... sigh"
 sleep $sleep_time
-../oflops -c veth1 -d veth3 -d veth5 ../example_modules/oflops_debug/.libs/liboflops_debug.so
+../.libs/oflops -c lo -d veth3 -d veth5 ../example_modules/oflops_debug/.libs/liboflops_debug.so
 ./test_teardown.sh
 
