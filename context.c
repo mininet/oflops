fb8f56be2466029faad284b9670fa14bbae27e37
Rob Sherwood <rob.sherwood@stanford.edu>
2009-09-14 15:12:54 -0700

    BUG: snaplen -1 is invalid; set to a high number
    Grr!  Hard to find...

--- context.c
+++ context.c
@@ -22,7 +22,7 @@ oflops_context * oflops_default_context(void)
 	ctx->listen_port = OFP_TCP_PORT;	// listen on default port
 
 	ctx->listen_fd   = -1;
-	ctx->snaplen = -1;
+	ctx->snaplen = 65535;
 
 	ctx->n_channels=1;
 	ctx->max_channels=10;
