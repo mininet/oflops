bdff42d23e5b7ce4691ac6cfcbf07d6d129f06b3
KK Yap <ykk@kkpc.Stanford.EDU>
2009-09-28 12:26:34 -0700

    Main page for documentation

--- oflops.h
+++ oflops.h
@@ -1,6 +1,23 @@
 #ifndef OFLOPS_H
 #define OFLOPS_H
 
+/** \mainpage oflops 
+ *
+ * oflops (OpenFlow operations per second) is created to 
+ * benchmark an OpenFlow switch.
+ *
+ * @author Rob Sherwood
+ * @date September 2009
+ *
+ * \section example_modules Example modules
+ * <UL>
+ * <LI> debug.c is used for debugging oflops </LI>
+ * <LI> pktin.c is test the number of packet in that can be
+ *      provided by a switch and the associated delay. </LI>
+ * </UL>
+ */
+
+
 #include "context.h"
 #include "test_module.h"
 
