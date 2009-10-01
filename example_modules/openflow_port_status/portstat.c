97b336ce67a8278bda433aa2e48bc56a8cfdae42
Rob Sherwood <rob.sherwood@stanford.edu>
2009-10-01 11:49:48 -0700

    radically changed portstats module
    made it send multiple packets at once
    seems to do useful things now
    
    aslo added an m4 dir so stupid verisons of autoconf
    	would not complain

--- example_modules/openflow_port_status/portstat.c
+++ example_modules/openflow_port_status/portstat.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,46 +17,53 @@
  */
 //#define MIN_SEND_INTERVAL 500000
 //#define MIN_SEND_INTERVAL 2000
-#define MIN_SEND_INTERVAL 2000
+int min_send_interval= 2000;
 
 /** String for scheduling events
  */
-#define BYESTR "bye bye"
+#define NEWRATE "newrate"
 #define WRITEPACKET "write packet"
+
+typedef struct probe {
+	int in_use;
+	struct timeval sent;
+} probe;
+
+#ifndef BUFLEN
+#define BUFLEN 4096
+#endif
+
+#define MAX_OUTSTANDING 65536
+probe probes[MAX_OUTSTANDING];
+
+
+int probes_per_second=1;
+int seconds_per_rate=5;
+int max_rate=1024;
+struct timeval starttime, end_time;
 
 /** OpenFlow packet buffer
  */
 struct ofp_stats_request buf;
 
-/** Start time
+/** index of next probe
  */
-struct timeval starttime;
-
-/** Sending.
- */
-int sending = 0;
-/** Send xid
- */
-uint32_t sendxid;
-/** Send time
- */
-struct timeval sendtime;
-
+int next_index=0;
 /** Send counter
  */
-uint64_t sendcounter = 0;
+uint64_t sentcounter = 0;
+uint64_t pcap_sentcounter = 0;
 /** Receive counter
  */
 uint64_t receivecounter = 0;
 /** Total delay
  */
-uint64_t totaldelay = 0;
 /** Delay packet counter
  */
-uint64_t delaycounter = 0;
+uint64_t totaldelay = 0;
 /** Delay file
  */
-FILE* delayfile;
+FILE* delayfile =NULL;
 
 /** Packet in module.
  * The module sends packet into a port to generate packet-in events.
@@ -69,7 +77,47 @@ FILE* delayfile;
  */
 char * name()
 {
-	return "Pkt_in_module";
+	return "port_status_module";
+}
+
+void new_rate(oflops_context * ctx)	// worst side effect evar
+{
+	char fbuf[BUFLEN];
+	struct timeval now;
+	int i;
+
+	if(delayfile)	
+	{
+		for(i=0;i<next_index;i++)
+		{
+			if(probes[i].in_use)	// expire unreturned probes
+				fprintf(delayfile,"%d -1\n",i);
+		}
+		fclose(delayfile);
+	}
+	next_index=0;
+	probes_per_second*=2;
+
+	if(probes_per_second > max_rate)
+	{
+		gettimeofday(&now,NULL);
+		//End experiment
+		fprintf(stderr, "Experiment has %lld packets sent and %lld received -- %f dropped",
+				sentcounter, receivecounter , (sentcounter-receivecounter)/(float)sentcounter);
+		fprintf(stderr, " (i.e., loss = %lld) with average delay of %f us.\n", 
+				(sentcounter-receivecounter),
+				((float) ((double) totaldelay)/((double) receivecounter)));
+		oflops_end_test(ctx);
+		return;
+	}
+	//Open delay file
+	snprintf(fbuf,BUFLEN,"statdelayfile.rate=%d",probes_per_second);
+	delayfile = fopen(fbuf, "w+");
+	fprintf(stderr,"---- Sending port_stats_requests at rate %d per second for %d seconds\n", probes_per_second, seconds_per_rate);
+	gettimeofday(&now,NULL);
+	end_time = now;
+	end_time.tv_sec += seconds_per_rate;
+	oflops_schedule_timer_event(ctx,&now, WRITEPACKET);
 }
 
 /** Initialization
@@ -77,44 +125,32 @@ char * name()
  */
 int start(struct oflops_context * ctx)
 {
-  struct timeval now;
-  struct ofp_header ofph;
-  gettimeofday(&now, NULL);
+	struct ofp_header ofph;
+	gettimeofday(&starttime,NULL);
+	starttime.tv_sec += 10;
 
-  //Open delay file
-  delayfile = fopen("statdelayfile", "w");
+	bzero(probes,sizeof(probe)*MAX_OUTSTANDING);
 
-  //Schedule start
-  now.tv_sec +=10;	
-  oflops_schedule_timer_event(ctx,&now, WRITEPACKET);
-  
-  //Schedule end
-  now.tv_sec += 60;	// 1 min on the future, stop this module
-  oflops_schedule_timer_event(ctx,&now, BYESTR);
+	
+	oflops_schedule_timer_event(ctx,&starttime, NEWRATE);
+	//Schedule first event
 
-  // send a friendly hello
-  ofph.length = htons(sizeof(struct ofp_header));
-  ofph.xid = 0;
-  ofph.type = OFPT_HELLO;
-  ofph.version = OFP_VERSION;
-  oflops_send_of_mesg(ctx,&ofph);
+	// send a friendly hello
+	ofph.length = htons(sizeof(struct ofp_header));
+	ofph.xid = 0;
+	ofph.type = OFPT_HELLO;
+	ofph.version = OFP_VERSION;
+	oflops_send_of_mesg(ctx,&ofph);
 
-  // send a features request, to stave off timeout (ignore response)
-  ofph.length = htons(sizeof(struct ofp_header));
-  ofph.xid = 0;
-  ofph.type = OFPT_FEATURES_REQUEST;
-  ofph.version = OFP_VERSION;
-  oflops_send_of_mesg(ctx,&ofph);
+	// build port stat request
+	buf.header.length = htons(sizeof(struct ofp_stats_request));
+	buf.header.xid = 0;
+	buf.header.type = OFPT_STATS_REQUEST;
+	buf.header.version = OFP_VERSION;
+	buf.type = htons(OFPST_PORT);
+	buf.flags = 0;
 
-  // build port stat request
-  buf.header.length = htons(sizeof(struct ofp_stats_request));
-  buf.header.xid = 0;
-  buf.header.type = OFPT_STATS_REQUEST;
-  buf.header.version = OFP_VERSION;
-  buf.type = htons(OFPST_PORT);
-  buf.flags = 0;
-
-  return 0;
+	return 0;
 }
 
 /** Handle timer event
@@ -123,54 +159,57 @@ int start(struct oflops_context * ctx)
  */
 int handle_timer_event(struct oflops_context * ctx, struct timer_event *te)
 {
-  struct timeval now;
-  char * str;
+	struct timeval now;
+	char * str;
 
-  gettimeofday(&now,NULL);
-  str = (char *) te->arg;
-  if (PACKET_IN_DEBUG)
-    fprintf(stderr, "At %ld.%.6ld (sched for %ld.%.6ld) : Got timer_event %s \n",
-	    now.tv_sec, now.tv_usec, te->sched_time.tv_sec, te->sched_time.tv_usec, str);
- 
-  if(!strcmp(str,WRITEPACKET))
-  {
-    //Send message
-    if (sendcounter == 0)
-    {
-      gettimeofday(&starttime, NULL);
-      fprintf(stderr, "Start...\n");
-    }
-    if (!sending)
-    {
-      buf.header.xid = htonl((uint32_t) sendcounter);
-      if (PACKET_IN_DEBUG)
-	fprintf(stderr, "Sending message %lld\n", sendcounter);
-      oflops_send_of_mesg(ctx,(struct ofp_header *) &buf);
-    }
+	gettimeofday(&now,NULL);
+	str = (char *) te->arg;
+	if (PACKET_IN_DEBUG)
+		fprintf(stderr, "At %ld.%.6ld (sched for %ld.%.6ld) : Got timer_event %s \n",
+				now.tv_sec, now.tv_usec, te->sched_time.tv_sec, te->sched_time.tv_usec, str);
 
-    //Schedule next one
-    now.tv_usec += MIN_SEND_INTERVAL;	
-    oflops_schedule_timer_event(ctx,&now, WRITEPACKET);
-  }
-  else if(!strcmp(str,BYESTR))
-  {
-    //End experiment
-    if (sending)
-      sendcounter--;
-    fprintf(stderr, "Experiment has %lld packets sent (rate %f) and %lld received",
-	    sendcounter,
-	    (float) (((double) sendcounter)/((double) (now.tv_sec - starttime.tv_sec))),
-	    receivecounter);
-    fprintf(stderr, " (i.e., loss = %lld) with average delay of %f us.\n", 
-	    (sendcounter-receivecounter),
-	    ((float) ((double) totaldelay)/((double) delaycounter)));
-    fclose(delayfile);
-    oflops_end_test(ctx);
-  }
-  else
-    fprintf(stderr, "Unknown timer event: %s", str);
-  return 0;
+	if(!strcmp(str,WRITEPACKET))
+	{
+		//Send message
+		if (sentcounter == 0)
+		{
+			gettimeofday(&starttime, NULL);
+			fprintf(stderr, "Start...\n");
+		}
+		buf.header.xid = next_index;
+		if (PACKET_IN_DEBUG)
+			fprintf(stderr, "Sending message %lld\n", sentcounter);
+		if(probes[next_index].in_use)
+			fprintf(delayfile,"%d %d\n",next_index, -1);
+		bzero(&probes[next_index],sizeof(probe));
+		probes[next_index].in_use=1;
+		next_index++;
+		next_index %= MAX_OUTSTANDING;
+		oflops_send_of_mesg(ctx,(struct ofp_header *) &buf);
+		sentcounter++;
+		//Schedule next one; using sched time to avoid creep
+		te->sched_time.tv_usec += 1000000/probes_per_second;;	
+		if(te->sched_time.tv_usec >= 1000000)
+		{
+			te->sched_time.tv_sec++;
+			te->sched_time.tv_usec-=1000000;
+		}
+		if(timercmp(&end_time, &te->sched_time, >))
+			// have time to fire off another probe
+			oflops_schedule_timer_event(ctx,&te->sched_time, WRITEPACKET);
+		else
+		{
+			te->sched_time.tv_sec++;	// give some time for probes to drain
+			oflops_schedule_timer_event(ctx,&te->sched_time, NEWRATE);
+		}
+	}
+	else if(!strcmp(str,NEWRATE))
+		new_rate(ctx);
+	else
+		fprintf(stderr, "Unknown timer event: %s", str);
+	return 0;
 }
+
 
 /** Handle timer event
  * @param ctx pointer to opaque context
@@ -203,75 +242,75 @@ int get_pcap_filter(struct oflops_context *ctx, oflops_channel_name ofc, char * 
  */
 int handle_pcap_event(struct oflops_context *ctx, struct pcap_event * pe, oflops_channel_name ch)
 {
-  if (ch == OFLOPS_CONTROL)
-  {
-    //See packet received
-    uint8_t type = pe->data[67];
-    struct timeval ptime = pe->pcaphdr.ts;
-    uint32_t xid  = ntohl(*((uint32_t*) &(pe->data)[70]));
-    if (PACKET_IN_DEBUG)
-      fprintf(stderr, "Got OpenFlow packet of length %u type %u at %ld.%.6ld of xid %u\n", 
-	      pe->pcaphdr.len, type,
-	      ptime.tv_sec, ptime.tv_usec, 
-	      xid);
+	uint8_t type;
+	struct timeval ptime;
+	int probe_index;
+	probe * p;
+	if (ch != OFLOPS_CONTROL)
+	{
+		fprintf(stderr, "wtf! why channel %u?", ch);
+		return 0;
+	}
+	//See packet received
+	if(pe->pcaphdr.caplen <70)
+		return 0;	// not for us
+	type = pe->data[67];
+	if(type != 16 && type != 17)
+		return 0;
+	ptime = pe->pcaphdr.ts;
+	probe_index  = *((uint32_t*) &(pe->data)[70]);
+	assert(probe_index>=0);
+	assert(probe_index<MAX_OUTSTANDING);
+	p = &probes[probe_index];
+	assert(p->in_use);
+	if (PACKET_IN_DEBUG)
+		fprintf(stderr, "Got OpenFlow packet of length %u type %u at %ld.%.6ld of probe_index %d\n", 
+				pe->pcaphdr.len, type,
+				ptime.tv_sec, ptime.tv_usec, 
+				probe_index);
 
-    //Handle stat request and stat reply
-    if (type == 16)
-    {
-      //Record sending
-      sending = 1;
-      sendtime = ptime;
-      sendxid = xid;
-      sendcounter++;
-      if (PACKET_IN_DEBUG)
-	fprintf(stderr, "Send stat request at %ld.%.6ld of xid %u\n", 
-		ptime.tv_sec, ptime.tv_usec, 
-		xid);
-    }
-    else if (type ==17)
-    {
-      sending = 0;
-      if (PACKET_IN_DEBUG)
-	fprintf(stderr, "Receive stat reply at %ld.%.6ld of xid %u\n", 
-		ptime.tv_sec, ptime.tv_usec, 
-		xid);
+	//Handle stat request and stat reply
+	if (type == 16)
+	{
+		//Record sending
+		p->sent = ptime;
+		pcap_sentcounter++;
+		if (PACKET_IN_DEBUG)
+			fprintf(stderr, "Send stat request at %ld.%.6ld of probe_index %d\n", 
+					ptime.tv_sec, ptime.tv_usec, 
+					probe_index);
+	}
+	else if (type ==17)
+	{
+		
+		if (PACKET_IN_DEBUG)
+			fprintf(stderr, "Receive stat reply at %ld.%.6ld of probe_index %d\n", 
+					ptime.tv_sec, ptime.tv_usec, 
+					probe_index);
 
-      //Check xid
-      if (xid != sendxid)
-      {
-	fprintf(stderr, "Send xid %u and receive xid %u, wtf?!",
-		sendxid, xid);
+		//Calculate time difference
+		struct timeval timediff;
+		timersub(&ptime, &p->sent, &timediff);
+		if (timediff.tv_sec != 0)
+		{
+			fprintf(stderr, "Delay of > 1 sec!\n");
+			return 0;
+		}
+		fprintf(delayfile, "%d %ld.%.6ld\n", probe_index, timediff.tv_sec, timediff.tv_usec);
+		totaldelay += (uint64_t) timediff.tv_usec;
+		receivecounter++;
+		p->in_use=0;
+
+		if (PACKET_IN_DEBUG)
+		{
+			fprintf(stderr, "Got stat of probe_index %d with delay %ld.%.6ld\n", 
+					probe_index,
+					timediff.tv_sec, timediff.tv_usec);
+			fprintf(stderr, "\twith %lld packets sent and %lld received.\n", 
+					sentcounter, receivecounter);
+		}
+	}
 	return 0;
-      }
-
-      //Calculate time difference
-      struct timeval timediff;
-      timersub(&ptime, &sendtime, &timediff);
-      if (timediff.tv_sec != 0)
-      {
-	  fprintf(stderr, "Delay of > 1 sec!");
-	  return 0;
-      }
-      fprintf(delayfile, "%ld\n", timediff.tv_usec);
-      totaldelay += (uint64_t) timediff.tv_usec;
-      delaycounter++;      
-      receivecounter++;
-
-      if (PACKET_IN_DEBUG)
-      {
-	fprintf(stderr, "Got stat of xid %u with delay %ld.%.6ld\n", 
-	    xid,
-	    timediff.tv_sec, timediff.tv_usec);
-	fprintf(stderr, "\twith %lld packets sent and %lld received.\n", 
-		sendcounter, receivecounter);
-      }
-    }
-  }
-
-  else
-    fprintf(stderr, "wtf! why channel %u?", ch);
-
-  return 0;
 }
 
 
